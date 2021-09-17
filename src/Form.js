b29a3ae6010a49f362912998de1d3cebaf42b4a2
Cooper Jackson <davisjaxon@gmail.com>
2021-09-17 01:37:41 -0700

    fixes initial form input weirdnesses and general styling

--- src/Form.js
+++ src/Form.js
@@ -24,7 +24,7 @@ export default function Form (props) {
     return (
         <form>
             <div>
-                <label>Recipe Name: </label>
+                <label>Recipe Name</label>
                 <input 
                     type="text"
                     name="recipe"
@@ -32,29 +32,72 @@ export default function Form (props) {
                 />
             </div>
             <div>
-                <label>Ingredients: </label>
-                <div className="input-button-container">
-                    <input 
-                        type="text"
-                        name="ingredient-unit-amount"
-                        onChange={onChange}
-                        id="units-selection"
-                    />
-                    <select name="ingredient-unit" onChange={onChange}>
-                        <option value="teaspoon">Teaspoon</option>
-                        <option value="tablespoon">Tablespoon</option>
-                        <option value="fluid ounce">Fluid Ounce</option>
-                        <option value="cup">Cup</option>
-                        <option value="pint">Pint</option>
-                        <option value="quart">Quart</option>
-                        <option value="gallon">Gallon</option>
-                    </select>
-                    <input 
-                        type="text"
-                        name="ingredient"
-                        onChange={onChange}
-                    />
-                    <button onClick={addIngredient} style={{display: 'inline-block'}}>+</button>
+                <div className="input-container">
+                    <div className="time-container">
+                        <label>Prep Time</label>
+                        <input 
+                            type="text"
+                            name="prep-time"
+                            onChange={onChange}
+                            id="prep-time"
+                        />
+                    </div>
+                    <div className="time-container">
+                        <label>Cook Time</label>
+                        <input 
+                            type="text"
+                            name="cook-time"
+                            onChange={onChange}
+                            id="cook-time"
+                        />
+                    </div>
+                    <div className="time-container">
+                        <label>Serves</label>
+                        <input 
+                            type="text"
+                            name="serves-number"
+                            onChange={onChange}
+                            id="serves-number"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <label>Add Ingredient</label>
+                <div className="input-container">
+                    <div className="input-div" id="units-selection">
+                        <label>Amount</label>
+                        <input 
+                            type="text"
+                            name="ingredient-unit-amount"
+                            onChange={onChange}
+                            id="ingredient-unit-amount-input"
+                        />
+                    </div>
+                    <div className="input-div">
+                        <label>Unit</label>
+                        <select name="ingredient-unit" onChange={onChange}>
+                            <option value="teaspoon">Teaspoon</option>
+                            <option value="tablespoon">Tablespoon</option>
+                            <option value="fluid ounce">Fluid Ounce</option>
+                            <option value="cup">Cup</option>
+                            <option value="pint">Pint</option>
+                            <option value="quart">Quart</option>
+                            <option value="gallon">Gallon</option>
+                        </select>
+                    </div>
+                    <div className="input-div">
+                        <label>Ingredient</label>
+                        <input 
+                            type="text"
+                            name="ingredient"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="input-div">
+                        <label>&nbsp;</label>
+                        <button onClick={addIngredient} style={{display: 'inline-block'}}>+</button>
+                    </div>
                 </div>
 
             </div>
@@ -62,8 +105,8 @@ export default function Form (props) {
 
             </section>
             <div>
-                <label>Instructions: </label>
-                <div className="input-button-container">
+                <label>Add Instruction</label>
+                <div className="input-container">
                     <input 
                         type="text"
                         name="instruction"
